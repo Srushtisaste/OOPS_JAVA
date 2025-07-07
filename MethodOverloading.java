d2f26708f102ca66c915bc0acf97308210ac8820
srushti saste <srushti.saste20@gmail.com>
2025-07-07 22:45:35 +0530

    Polimorphism....Method Overloading

---
+++ MethodOverloading.java
@@ -0,0 +1,24 @@
+public class MethodOverloading{
+    public static void main(String [] args){
+        Calculator cal = new Calculator();
+       System.out.println(cal.sum(2,3));
+       System.out.println(cal.sum((float)20,(float)30.5));
+       System.out.println(cal.sum(2,3,6));
+
+
+    }
+}
+
+class Calculator{
+    int sum(int a,int b){
+        return a+b;
+    }
+
+    float sum(float a , float b){
+        return a+b;
+    }
+
+    int sum(int a,int b ,int c){
+        return a+b+c;
+    }
+}
