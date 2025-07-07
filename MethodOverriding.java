7046fdfccdb2d58a4ace801c66d6e61056cd5bbf
srushti saste <srushti.saste20@gmail.com>
2025-07-07 22:52:56 +0530

    Polymorphism...method overriding

---
+++ MethodOverriding.java
@@ -0,0 +1,23 @@
+public class MethodOverriding{
+
+    public static void main(String[] args){
+        Deer d = new Deer();
+            d.eat();
+
+        
+
+    }
+
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eat anything...");
+    }
+}
+
+class Deer{
+    void eat(){
+        System.out.println("Eat Grass..");
+    }
+}
